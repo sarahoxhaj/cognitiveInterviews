0baa9da2fc94618f485df391e703205d12796009
sara hoxhaj <12242040@univie.ac.at>
2026-08-18 11:04:36 +0200

    changing to simple language

--- src/components/ConsentForm.vue
+++ src/components/ConsentForm.vue
@@ -1,7 +1,8 @@
 <template>
 
     <div class="bg-gray-50 dark:bg-slate-600 flex flex-col min-h-screen">
-        <p class="mb-2 text-2xl tracking-tight text-gray-900 dark:text-white mt-7 text-center">Consent form</p>
+        <p class="mb-2 text-2xl tracking-tight text-gray-900 dark:text-white mt-7 text-center">Before You Take Part...
+        </p>
         <div class="text-left px-24 dark:text-white">
             <!-- <p><b>Introduction</b></p>
             <p>You are invited to participate in a study titled "A Validated Scale for Visual Complexity in
@@ -19,35 +20,30 @@
             <p class="pl-6">• who is fluent in English</p>
             <p class="pl-6">• is 18 years or older, and</p>
             <p class="pl-6">• considers himself/herself an expert on the topic of visualization</p> -->
-            <p class="mt-2"><b>What Participation Involves</b></p>
-            <p>This study is part of a research project evaluating how people perceive the visual complexity of a data
-                visualization. The session is a cognitive interview and will take approximately 30 minutes.<br>
-                Participation involves a think-aloud task, where you will be asked to verbalize your thoughts while
-                rating
-                16 items on a Likert scale evaluating visual complexity. Before the main task, you will complete a
-                short training exercise to get familiar with the think-aloud method. This part is not analyzed as data.
-                The full session (excluding the training task) will be <u>audio and screen-recorded</u> for data
-                collection
-                and quality-review purposes. The survey also includes a brief background questionnaire.
+            <p class="mt-2"><b>In this survey you will</b> be asked to rate 16 statements from <i>strongly disagree</i>
+                to <i>strongly agree</i> and say out loud what you are thinking as you rating each statement. Before
+                starting, you will complete a short task only to get familiar with this method. The full session
+                (excluding the training task) will be audio and screen-recorded. This is done only for data collection
+                and quality-review purposes.
             </p>
-            <p class="mt-2"><b>Voluntariness of participation</b></p>
-            <p>Participation is voluntary. You may stop participating at any time without giving a reason. If you
-                withdraw, your data will not be included in the analysis, and there will be no consequences.</p>
-            <p class="mt-2"><b>Anonymity and Confidentiality</b></p>
-            <p>We do not collect any personal identifying information such as your name or email. However, because the
-                session includes audio and screen recording, your voice (and any content visible on the screen) may be
-                identifiable to the research team. Only the research team will have access to the raw recordings, which
-                will be stored securely and deleted after the analysis phase is complete.
-                Any results shared publicly will be anonymized and will not include identifiable
-                audio, video, or screen content.</p>
-            <p class="mt-2"><b>Risks/Discomforts</b></p>
+
+            <p class="mt-2">Taking part is <b>voluntary</b>, and you can stop aty any time without giving a reason. If
+                you choose to stop, your data will not be included in the analysis and there will be no consequences.
+            </p>
+
+            <p class="mt-2">We <b>do not collect</b> any information that can <b>identify</b> you, such as your name or
+                email address. However, because the conversation is recorded, your voice may be recognizable to the
+                research team. Only the research team will have access to the recordings, and they will be deleted after
+                the analysis is complete. Any results shared publicly will be anonymized and will not include the
+                recordings.</p>
+            <!-- <p class="mt-2"><b>Risks/Discomforts</b></p>
             <p>This research study involves minimal risk. The main risk is the small possibility of a data breach, which
                 we minimize by not collecting any personal identifying information and storing data securely on
                 password-protected computers. Study results will be published in an academic paper and
-                anonymized responses may be shared publicly.</p>
-            <p class="mt-2"><b>Compensation</b></p>
-            <p>Participants will receive no compensation for their participation.</p>
-            <p class="mt-2"><b>Contact details</b></p>
+                anonymized responses may be shared publicly.</p> -->
+            <!-- <p class="mt-2"><b>Compensation</b></p>
+            <p>Participants will receive no compensation for their participation.</p> -->
+            <!-- <p class="mt-2"><b>Contact details</b></p>
             <p>Please feel free to contact one of the six researchers below if you wish to know more about the study,
                 or if you have any questions:</p>
             <div class="flex justify-between w-full mt-2">
@@ -75,16 +71,16 @@
                     <p><b>Anne-Flore Cabouat</b></p> <a href="mailto:anne-flore.cabouat@inria.fr"
                         class="font-medium text-blue-600 dark:text-amber-300 hover:underline">anne-flore.cabouat@inria.fr</a>
                 </div>
-            </div>
+            </div> -->
             <div class="mt-6 bg-sky-900 rounded-lg py-4">
                 <p class="px-2">
                     <span class="text-red-500 text-2xl">* </span>
                     <span class="text-white">Please confirm that you wish to participate in this study.</span>
                 </p>
-                <p class="text-white px-2">By clicking the <b>Yes</b> button below, you certify they you have read and
-                    understood the above information, that you meet the various selection criteria, that your questions
-                    have been answered satisfactorily, and that you have been advised that you are free to withdraw from
-                    this research at any time, without any consequence.</p>
+                <p class="text-white px-2">By clicking the <b>Yes</b> button below, you confirm that you have read and
+                    understood the information above, that you meet the requirements for taking part, and that your
+                    questions have been answered. You understand that taking part is voluntary and that you can stop at
+                    any time without giving a reason or facing any consequences.</p>
                 <div class="flex items-center space-x-4 px-2 ml-4 text-white mt-3">
                     <label class="flex items-center">
                         <input type="radio" name="choice" value="yes" class="mr-2">
--- src/components/FirstView.vue
+++ src/components/FirstView.vue
@@ -28,20 +28,19 @@
 
     <div class="bg-gray-50 dark:bg-slate-600 flex flex-col items-center justify-start min-h-screen">
         <p class="mb-2 text-2xl tracking-tight text-gray-900 dark:text-white mt-14">Visual Complexity in Data
-            Visualizations: Scale Development</p>
+            Visualizations</p>
         <p class="mt-5 max-w-5xl text-center dark:text-white">
-            You are invited to participate in our study that contributes to the project "A Validated Scale for Visual
-            Complexity in
-            Data Visualizations". <br>
-            Our goal is to develop a validated scale that can be used to evaluate the visual complexity of a data
+            Do you notice when a design or image feels <i>"too much"</i> to look at? We want to find out what makes it
+            feel that way. Our goal is to develop a scale that can be used to evaluate the visual complexity of a data
             visualization.</p>
         <p class="mt-5 max-w-5xl text-center dark:text-white
             ">
-            <b>What this involves:</b> a ~30-minute online session where you will think aloud while rating a data
-            visualization. <br>The session will be audio- and screen-recorded.
+            This conversation will last around 30 minutes and includes a think-aloud process where you express your
+            thought while answering the questions.<br> The audio and the screen will be recorded only for data
+            collection and quality-review purposes.
         </p>
         <p class="mt-5 dark:text-white">
-            Please respond to this survey only if you:<br>
+            If you are interested to continue and:<br>
         </p>
         <div class="mt-2 dark:text-white max-w-xl mx-auto text-left">
             • are 18 years or older, <br>
@@ -49,7 +48,7 @@
             • feel comfortable answering questions in English
         </div>
 
-        <p class="mt-7 dark:text-white">
+        <!-- <p class="mt-7 dark:text-white">
             This study is conducted by:<br>
         </p>
         <div class="mt-2 dark:text-white max-w-xl mx-auto text-left">
@@ -65,10 +64,10 @@
                 Paris-Saclay)</a><br>
             • <a href="mailto:anne-flore.cabouat@inria.fr" class="hover:text-blue-500">Anne-Flore Cabouat (University of
                 Paris-Saclay)</a>
-        </div>
-        <p class="mt-4 dark:text-white">Click <button @click.prevent="goToConsentForm" type="submit"
+        </div> -->
+        <p class="mt-4 dark:text-white">Please click <button @click.prevent="goToConsentForm" type="submit"
                 class="ml-2 mr-2 py-2 px-2 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700  dark:text-black dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-400">Next</button>
-            if you are interested to continue.</p>
+        </p>
     </div>
 
 </template>
